adbe9cf808157e1260f5906821610e43bd8e43e7
vipul <vipul21sept@gmail.com>
2018-05-28 18:06:35 +0530

    Changes in skeleton component - Added attributes

--- src/Skeleton/Skeleton.js
+++ src/Skeleton/Skeleton.js
@@ -4,20 +4,22 @@ import '../utils/style.css';
 
 class Skeleton extends Component {
   render() {
-    // TODO: To apply all the custom styles to the component
+    // TODO: To apply all the custom styles to the component. 2 left - widthRandomness and heightRandomness
     const style = {
-      display: this.props.display,
       width: this.props.width,
       height: this.props.height,
       backgroundColor: this.props.backgroundColor,
-      backgroundImage: `linear-gradient(90deg, ${this.props.backgroundColor}, #f5f5f5, ${this.props.backgroundColor})`,
+      backgroundImage: `linear-gradient(90deg, ${this.props.backgroundColor}, ${this.props.gradientColor}, ${this.props.backgroundColor})`,
     };
     if(this.props.animate) {
       style.animation = `slide ${this.props.speed}s ease-in-out infinite`;
     }
+    if(!this.props.display) {
+      style.display = `none`;
+    }
     const className=`react-skeleton ${this.props.className}`;
     return (
-      <div style={style} className={className}>
+      <div style={[style,...this.props.style]} className={className}>
         {this.props.children}
       </div>
     );
@@ -36,14 +38,16 @@ Skeleton.propTypes = {
   heightRandomness: PropTypes.number,
   animate: PropTypes.bool,
   className: PropTypes.string,
-  children: PropTypes.any
+  children: PropTypes.any,
 };
 
+// TODO: default propTypes for others attributes.
 Skeleton.defaultProps = {
-  display: false,
+  display: true,
   speed: 1.2,
   animation: true,
-
+  backgroundColor: '#eee',
+  gradientColor: '#f5f5f5'
 }
 
 export default Skeleton;
