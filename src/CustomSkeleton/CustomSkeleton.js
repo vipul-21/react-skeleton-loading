1c0129a745708c2ed989fac07430d5be768d638b
vipul <vipul21sept@gmail.com>
2018-06-01 10:57:53 +0530

    Removing es6 Syntax

--- src/CustomSkeleton/CustomSkeleton.js
+++ src/CustomSkeleton/CustomSkeleton.js
@@ -4,22 +4,22 @@ import PropTypes from 'prop-types';
 
 class CustomSkeleton extends Component {
   render() {
-    let style = `div[skeleton] {
-      background-color: ${this.props.backgroundColor},
-      backgroundImage: linear-gradient(90deg, ${this.props.backgroundColor}, #f5f5f5, ${this.props.backgroundColor}),
-    }`;
-    // TODO: For every property and find a better solution
-    if(this.props.style) {
-      // TODO: For style
-    }
-    if(!this.props.display) {
-      // TODO: For display
-    }
+    // let style = `div[skeleton] {
+    //   background-color: ${this.props.backgroundColor},
+    //   backgroundImage: linear-gradient(90deg, ${this.props.backgroundColor}, #f5f5f5, ${this.props.backgroundColor}),
+    // }`;
+    // // TODO: For every property and find a better solution
+    // if(this.props.style) {
+    //   // TODO: For style
+    // }
+    // if(!this.props.display) {
+    //   // TODO: For display
+    // }
 
     return (
       <div>
         <style type="text/css">
-          {style}
+          {/* {style} */}
         </style>
         {this.props.component}
       </div>
